534f367b7108d435ab7c40f3a7cc4e523131dcda
solomonrazak <solomonrazak99@gmail.com>
2024-08-16 19:03:38 -0700

    updated

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './hero.scss';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="wrapper">
+        <div className="textContainer">
+            <h2>SOLOMON RAZAK</h2>
+            <h1>Full Stack Software Engineer</h1>
+            <div className="buttons">
+                <button>See the Latest Works</button>
+                <button>Contact Me</button>
+
+            </div>
+            <img src="/scroll.png" alt=""/>
+        </div>
+        </div>
+        <div className="imageContainer">
+            <img src="/hero.png" alt=""/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
